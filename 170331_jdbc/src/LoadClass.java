1bbbb3d934d5b9a7343a9cd770ae1c56b0ad3878
baebyoungjoo <angel_bj@naver.com>
2017-03-31 14:19:12 +0900

    no message

---
+++ 170331_jdbc/src/LoadClass.java
@@ -0,0 +1,26 @@
+import java.sql.*;
+
+public class LoadClass {
+	Statement stmt1 = null;
+	// »ý¼ºÀÚ
+	LoadClass(Statement stmt){
+		this.stmt1 = stmt;
+	}
+	public void call() throws SQLException{
+		// find nearby particular latitude & longitude 
+		String str ;
+		double lat = 37.3860521 , lng = 127.1214038;
+		str = String.format("select * from wifi where SQRT(POWER(latitude-%f,2) + POWER(longitude-%f,2))="
+				+ "(select MIN(SQRT(POWER (latitude-%f,2) + POWER(longitude-%f,2))) from wifi);", lat,lng,lat,lng);
+		ResultSet rest = stmt1.executeQuery(str);
+		while(rest.next()){
+			// result print
+			System.out.println(
+					rest.getString(1) +"\t" + rest.getString(2) +"\t" + rest.getString(3)
+			+ "\t" + rest.getString(4) + "\t" + rest.getString(5) + "\t" + rest.getString(6)
+			+ "\t" + rest.getString(7) + "\t" + rest.getString(8) + "\t" + rest.getString(9)
+			+ "\t" + rest.getString(10) + "\t" + rest.getString(11) + "\t" + rest.getString(12)
+			+ "\t" + rest.getString(13) + "\t" + rest.getString(14) + "\t" + rest.getString(15));
+		}
+	}
+}
